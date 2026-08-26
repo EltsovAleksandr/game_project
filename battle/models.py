ca270d1b9cb48a501e0e98ab5ed76c88825b366f
EltsovAleksandr <Krasavcheg33.69@gmail.com>
2026-08-26 14:02:35 +0300

    Create game - battle

---
+++ battle/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+
+    score = models.IntegerField(verbose_name = 'Очки',default = 0)
+    level = models.IntegerField(verbose_name = 'Уровень', default = 1)
--- game_project/settings.py
+++ game_project/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'battle.CustomUser'
 
 # Application definition
 
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'battle',
+
+
 ]
 
 MIDDLEWARE = [
